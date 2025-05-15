a6a6429bec4d914d0160e03974c29f286b05110c
Yanxiao Zhao <pazyx728@gmail.com>
2025-05-15 18:34:18 +0800

    update setup.py & update Quickstart

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# SATQuest
-A Verifier for Logical Reasoning Evaluation and Reinforcement Fine-Tuning of LLMs
+# SATQuest: A Verifier for Logical Reasoning Evaluation and Reinforcement Fine-Tuning of LLMs
+
+## 🚀 Quickstart
+
+```python
+# !pip install datasets satquest
+from datasets import load_dataset
+from satquest import CNF, create_problem, create_question
+cnf = CNF(dimacs=load_dataset('sdpkjc/SATQuest', split='test')[0]['sat_dimacs'])
+# cnf.shuffle()
+P, Q = create_problem('SATSP', cnf), create_question('math')
+prompt = P.accept(Q)
+answer = P.solution # LLM(prompt)
+reward = int(P.check(answer))
+```
--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ setup(
     name="satquest",
     version="0.1.0",
     author="Yanxiao Zhao",
-    author_email="you@example.com",
+    author_email="pazyx728@gmail.com",
     description="SATQuest",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/sdpkjc/SATQuest",
     packages=find_packages("."),
     python_requires=">=3.9",
-    install_requires=[],
+    install_requires=[
+        "python-sat>=1.8.dev14",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
