4556f64549bb80e5b7a68480425566e2511db254
Adam Zhao <pazyx728@gmail.com>
2025-09-26 17:01:28 +0800

    fix

--- quickstart.py
+++ quickstart.py
@@ -1,22 +1,24 @@
 from datasets import load_dataset
+
 from satquest import CNF, create_problem, create_question
 
-item = load_dataset('sdpkjc/SATQuest', split='test')[0]
-cnf = CNF(dimacs=item['sat_dimacs'])
+item = load_dataset("sdpkjc/SATQuest", split="test")[0]
+cnf = CNF(dimacs=item["sat_dimacs"])
 # cnf.shuffle()
 
-problem = create_problem('SATSP', cnf) # or 'SATDP', 'MaxSAT', 'MCS', 'MUS'
-question = create_question('math')  # or 'dimacs', 'story', 'dualstory'
+problem = create_problem("SATSP", cnf)  # or 'SATDP', 'MaxSAT', 'MCS', 'MUS'
+question = create_question("math")  # or 'dimacs', 'story', 'dualstory'
+
 prompt = problem.accept(question)
 print(f"===== Prompt =====\n{prompt}\n")
-'''
+"""
 Given a CNF formula with 3 variables and 12 clauses in mathematical notation:
 
 (x_1 \lor x_2 \lor x_3) \land (x_3 \lor \neg x_1 \lor x_2) \land (x_1 \lor x_3 \lor \neg x_2) \land (x_1 \lor \neg x_2) \land (x_3 \lor x_1 \lor \neg x_2) \land (x_3 \lor \neg x_1 \lor x_2) \land (\neg x_3 \lor \neg x_1) \land (\neg x_1 \lor x_2 \lor x_3) \land (\neg x_2 \lor \neg x_3) \land (\neg x_2 \lor x_3 \lor x_1) \land (x_1 \lor \neg x_3) \land (\neg x_3 \lor \neg x_2 \lor \neg x_1)
 
 Find a satisfying assignment for the formula.
 Output a binary string of length 3 ('1' for true, '0' for false).
-'''
+"""
 
 answer = problem.solution  # reference answer
 print(f"===== Answer =====\n{answer}\n")
